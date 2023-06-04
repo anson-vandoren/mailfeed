83c89d967677ed16cf6e6b97fb2232e73a854433
Anson VanDoren <anson@ansonvandoren.com>
2023-06-04 16:10:23 -0700

    IT WORKS

--- mailfeed/src/email_sender.rs
+++ mailfeed/src/email_sender.rs
@@ -1,7 +1,13 @@
-use std::fmt::Debug;
+use std::{env, fmt::Debug};
 
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{TimeZone, Utc};
 use diesel::SqliteConnection;
+use lettre::{
+    error::Error,
+    message::{header::ContentType, MultiPart, SinglePart},
+    transport::smtp::authentication::Credentials,
+    Message, SmtpTransport, Transport,
+};
 
 use crate::{
     feed_monitor::FEED_CHECK_INTERVAL,
@@ -9,17 +15,53 @@ use crate::{
         feed::Feed,
         feed_item::FeedItem,
         subscription::{Frequency, Subscription},
-        user::{User, UserQuery},
+        user::User,
     },
     DbPool,
 };
 
+#[derive(Debug)]
+pub struct EmailServerCfg {
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub from_email: String,
+}
+
+fn get_server_config() -> EmailServerCfg {
+    let host = env::var("MF_SMTP_HOST").unwrap();
+    let port = env::var("MF_SMTP_PORT").unwrap().parse::<u16>().unwrap();
+    let username = env::var("MF_SMTP_USERNAME").unwrap();
+    let password = env::var("MF_SMTP_PASSWORD").unwrap();
+    let from_email = env::var("MF_FROM_EMAIL").unwrap();
+    EmailServerCfg {
+        host,
+        port,
+        username,
+        password,
+        from_email,
+    }
+}
+
 pub async fn start(pool: DbPool) {
+    let cfg = get_server_config();
+    let sender = match SmtpTransport::relay(&cfg.host) {
+        Ok(sender) => sender,
+        Err(e) => {
+            log::error!("Error creating email sender: {:?}", e);
+            return;
+        }
+    }
+    .credentials(Credentials::new(cfg.username, cfg.password))
+    .build();
+
     loop {
         let mut conn = match pool.get() {
             Ok(conn) => conn,
             Err(e) => {
                 log::error!("Error getting DB connection: {:?}", e);
+                tokio::time::sleep(FEED_CHECK_INTERVAL).await;
                 continue;
             }
         };
@@ -30,8 +72,30 @@ pub async fn start(pool: DbPool) {
 
         for user in users {
             let email_data = items_to_send_by_user(&mut conn, user.id);
-            let email_text = format_items_to_email(email_data);
-            log::info!("Sending email to {}:\n{}", user.send_email, email_text)
+            let email_html = to_html_email(&email_data);
+            let email_plain = to_plain_email(&email_data);
+            let message =
+                construct_email(&user.send_email, &cfg.from_email, &email_html, &email_plain);
+            let message = match message {
+                Ok(message) => message,
+                Err(e) => {
+                    log::error!("Error constructing email: {:?}", e);
+                    tokio::time::sleep(FEED_CHECK_INTERVAL).await;
+                    continue;
+                }
+            };
+            let email_result = sender.send(&message);
+            match email_result {
+                Ok(_) => {
+                    log::info!("Email sent to {}", user.send_email);
+                }
+                Err(e) => {
+                    log::error!("Error sending email: {:?}", e);
+                }
+            }
+
+            // TODO: mark having sent
+            panic!("TODO: mark having sent");
         }
 
         // TODO: move to the top
@@ -53,8 +117,6 @@ fn items_to_send_by_feed(
 
 #[derive(Debug)]
 struct FeedData {
-    feed_id: i32,
-    time_after: i32,
     new_items: Vec<FeedItem>,
     feed_title: String,
     feed_link: String,
@@ -62,14 +124,11 @@ struct FeedData {
 
 #[derive(Debug)]
 struct EmailData {
-    user_id: i32,
-    user_email: String,
     feed_data: Vec<FeedData>,
 }
 
 fn items_to_send_by_user(conn: &mut SqliteConnection, user_id: i32) -> EmailData {
     let subscriptions = Subscription::get_all_for_user(conn, user_id).unwrap();
-    let user_email = User::get(conn, UserQuery::Id(user_id)).unwrap().send_email;
     let mut feed_data = Vec::new();
     for sub in subscriptions {
         let feed_id = sub.feed_id;
@@ -96,30 +155,50 @@ fn items_to_send_by_user(conn: &mut SqliteConnection, user_id: i32) -> EmailData
 
         let new_items = items_to_send_by_feed(conn, feed_id, last_sent);
         feed_data.push(FeedData {
-            feed_id,
-            time_after: last_sent,
             new_items,
             feed_title: feed.title,
             feed_link: feed.url,
         });
     }
-    EmailData {
-        user_id,
-        feed_data,
-        user_email,
-    }
+    EmailData { feed_data }
 }
 
-fn format_items_to_email(email_data: EmailData) -> String {
+fn construct_email(
+    to_email: &str,
+    from_email: &str,
+    as_html: &str,
+    as_plain: &str,
+) -> Result<Message, Error> {
+    Message::builder()
+        // TODO: update this to env var
+        .from(from_email.parse().unwrap())
+        .to(to_email.parse().unwrap())
+        .subject("MailFeed Digest")
+        .multipart(
+            MultiPart::alternative()
+                .singlepart(
+                    SinglePart::builder()
+                        .header(ContentType::TEXT_PLAIN)
+                        .body(as_plain.to_string()),
+                )
+                .singlepart(
+                    SinglePart::builder()
+                        .header(ContentType::TEXT_HTML)
+                        .body(as_html.to_string()),
+                ),
+        )
+}
+
+fn to_html_email(email_data: &EmailData) -> String {
     // create mutable String `result`, initialized w/ EMAIL_TEMPLATE_HEAD
     let mut result = EMAIL_TEMPLATE_HEAD.to_string();
-    for feed_data in email_data.feed_data {
+    for feed_data in &email_data.feed_data {
         result.push_str(&format!(
             "<h2>{}</h2>
             <a href='{}'>View Feed</a>",
             feed_data.feed_title, feed_data.feed_link
         ));
-        for item in feed_data.new_items {
+        for item in &feed_data.new_items {
             let date_time = Utc.timestamp_opt(item.pub_date as i64, 0).unwrap();
             result.push_str(&format!(
                 "<div class='feed-item'>
@@ -131,15 +210,48 @@ fn format_items_to_email(email_data: EmailData) -> String {
                 item.link,
                 item.title,
                 item.description
+                    .clone()
                     .unwrap_or("No description provided".to_string()),
                 date_time.format("%Y-%m-%d %H:%M:%S"),
-                item.author.unwrap_or("No author provided".to_string())
+                item.author
+                    .clone()
+                    .unwrap_or("No author provided".to_string())
             ));
         }
         result.push_str("<hr />");
     }
     // push EMAIL_TEMPLATE_FOOT to `result`
     result.push_str(EMAIL_TEMPLATE_FOOT);
+    result
+}
+
+fn to_plain_email(email_data: &EmailData) -> String {
+    let mut result = "MailFeed Digest\n\n".to_string();
+    for feed_data in &email_data.feed_data {
+        result.push_str(&format!(
+            "{}\nView Feed: {}\n",
+            feed_data.feed_title, feed_data.feed_link
+        ));
+        for item in &feed_data.new_items {
+            let date_time = Utc.timestamp_opt(item.pub_date as i64, 0).unwrap();
+            let description = item
+                .description
+                .clone()
+                .unwrap_or("No description provided".to_string());
+
+            result.push_str(&format!(
+                "{}\n{}\n{}\n{}\n{}\n----------\n\n",
+                item.link,
+                item.title,
+                html_escape::decode_html_entities(&description),
+                date_time.format("%Y-%m-%d %H:%M:%S"),
+                item.author
+                    .clone()
+                    .unwrap_or("No author provided".to_string())
+            ));
+        }
+        result.push('\n');
+    }
     result
 }
 
